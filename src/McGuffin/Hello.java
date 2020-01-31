16de4151b031db597eb050286479183fd763fa51
IsabelDing00 <chongfan@byui.edu>
2020-01-31 16:39:43 -0700

    This is from Isabel.

--- src/McGuffin/Hello.java
+++ src/McGuffin/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args){
      System.out.println("Hello, my name is Robert");
      System.out.println("Hey Robert, its Reece! How is everyone else doing?");
+     System.out.println("This is Isabel, you guys are wonderful teammates!");
     }
 }
